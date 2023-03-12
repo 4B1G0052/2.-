901dd7d364ef1e9eafdcdab910f8c8339eaa5d26
4B1G0052 <4B1G0052@stust.edu.tw>
2023-03-12 23:04:29 +0800

    加入專案檔案。

---
+++ 2.英里轉公里/2.英里轉公里.cpp
@@ -0,0 +1,16 @@
+﻿
+#include<iostream>  
+#include <iomanip>  
+using namespace std;
+
+int main()
+{
+    int a;
+    while (cin >> a) {
+        double b = a * 1.6;
+        cout << fixed << setprecision(1) << b << "\n";
+    }
+
+    return 0;
+}
+
